a79a59b6ddd9a96b5c15cde86764d88ab5606507
Nicolas Massonneau <nmassonneau@gmail.com>
2019-02-27 22:18:52 +0100

    ajout du dl de vundle

--- install.bat
+++ install.bat
@@ -4,4 +4,5 @@ SET DIR=%~dp0
 cmd /c mklink /d "%LOCALAPPDATA%\nvim" "%DIR%\.config\nvim"
 cmd /c mklink "%USERPROFILE%\.vimrc" "%DIR%\.vimrc"
 cmd /c mklink /d "%USERPROFILE%\.vim" "%DIR%\.vim"
+git clone https://github.com/VundleVim/Vundle.vim.git %DIR%/.vim/bundle/Vundle.vim
 nvim --headless +PluginInstall +qa
